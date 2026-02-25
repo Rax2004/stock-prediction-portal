c782317ebd6524123d8c4d3694d927b17063e13a
Rax <anirudhmohan30@gmail.com>
2026-02-25 22:00:00 +0530

    Frontend Homepage done

---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Button from './Button'
+
+const Main = () => {
+  return (
+    <>
+      <div className='container'>
+        <div className='p-5 text-center bg-light-dark rounded'>
+          <h1 className='text-light'>Stock Prediction App</h1>
+          <p className='text-light lead'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores porro quos velit ipsam. Ipsum saepe iusto expedita magnam dignissimos nemo veniam fuga illo delectus, rerum esse facere eligendi rem mollitia.</p>
+          <Button text = 'Login' class='btn-outline-info'/>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Main
